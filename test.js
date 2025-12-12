adcbe573c356dbab9da98b681c305e71c1b0bd6c
arpitaeral-aru <arpitaeral@gmail.com>
2025-12-12 12:15:42 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,17 @@
+const name='arpita'
+const repoCount=45
+
+console.log(`my name is ${name} and my repoCount is ${repoCount}`);
+
+
+console.log(name.charAt(2));
+
+
+console.log(name.__proto__);
+
+
+console.log(name.indexOf('t'));
+
+console.log(name.length);
+
+console.log(name.toUpperCase());
